581278d87918fde023c6edeb70f1012076742baa
Albertomanas <amanas@cifpfbmoll.eu>
2020-05-28 16:40:51 +0200

    feat(FacturesService): Añadir producto y notificaición de regalo al cliente.

--- src/main/java/org/formacio/servei/FacturesService.java
+++ src/main/java/org/formacio/servei/FacturesService.java
@@ -1,11 +1,24 @@
 package org.formacio.servei;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.formacio.domain.Factura;
+import org.formacio.domain.LiniaFactura;
+import org.formacio.repositori.FacturesRepositori;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class FacturesService {
 
+	@Autowired
+	FacturesRepositori facturasRepo;
+	
+	@Autowired
+	FidalitzacioService fidalitzacioService;
 	
 	/*
 	 * Aquest metode ha de carregar la factura amb id idFactura i afegir una nova linia amb les dades
@@ -16,7 +29,26 @@ public class FacturesService {
 	 * Per implementar aquest metode necessitareu una referencia (dependencia) a FacturesRepositori
 	 */
 	public Factura afegirProducte (long idFactura, String producte, int totalProducte) {
+	
+		Optional<Factura> factura = facturasRepo.findById(idFactura);
+		if(factura.isPresent()) {
+			LiniaFactura liniaFactura = new LiniaFactura();
+			liniaFactura.setProducte(producte);
+			liniaFactura.setTotal(totalProducte);
+			factura.get().getLinies().add(liniaFactura);
+			facturasRepo.save(factura.get());
+			
+			
+			if(factura.get().getLinies().size() >= 4) {
+				String email = factura.get().getClient().getEmail();
+				fidalitzacioService.notificaRegal(email);
+			}
+			return factura.get();
+			
+		} else {
+			return null;
+		}
+
 		
-		return null;
 	}
 }
